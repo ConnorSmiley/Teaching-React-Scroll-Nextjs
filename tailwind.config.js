1ef2b0607431682b5ff6b23c328e766f2c611653
Connor Smiley <moxiesmiley@gmail.com>
2022-07-11 16:29:31 +0900

    v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,6 @@ module.exports = {
 
       black: colors.black,
       white: colors.white,
-      gray: colors.coolGray,
       red: colors.red,
       yellow: colors.amber,
       green: colors.emerald,
